4a6b469b28af3f331239311fad84da27c3b30133
arjunbector <arjunbector@gmail.com>
2024-01-22 17:27:25 +0530

    add components and ui

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="bg-[color:var(--background)] min-h-screen">
-      AGRISIM
+      
     </main>
   );
 }
