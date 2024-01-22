d98a0866df2efeb38cc558f1ba456381767c328f
arjunbector <arjunbector@gmail.com>
2024-01-22 18:31:40 +0530

    feat: add navbar, login, signup

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
 import logo from "../../public/assets/logos/logo.svg";
+import Link from "next/link";
 const Navbar = () => {
   return (
-    <nav className="w-full flex items-center justify-between">
+    <nav className="w-full flex items-center justify-center sm:justify-between">
       <div className="left h-14 w-1/2">
         <Image src={logo} alt="logo" className="h-full" />
       </div>
       <div className="hidden sm:block right w-1/2 font-medium text-xl">
         <ul className="flex gap-16 justify-end mr-10">
-          <li className="cursor-pointer">Home</li>
-          <li className="cursor-pointer">Farms</li>
-          <li className="cursor-pointer">About Us</li>
+          <Link href={"/"}><li className="cursor-pointer">Home</li></Link>
+          <Link href={"/"}><li className="cursor-pointer">Farms</li></Link>
+          <Link href={"/"}><li className="cursor-pointer">About Us</li></Link>
         </ul>
       </div>
     </nav>
