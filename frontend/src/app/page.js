885a1d4e04a43c41ceceb9b448f10e061f4c1f66
arjunbector <arjunbector@gmail.com>
2024-01-22 17:15:54 +0530

    add colot

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-[color:var(--background)] min-h-screen">
       AGRISIM
     </main>
   );
