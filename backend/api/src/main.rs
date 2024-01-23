d662ea66853a0508d605f61ed6b8c5832b5d4a8d
Soham Ghugare <soham.ghugare@gmail.com>
2024-01-23 10:37:50 +0530

    feat: added root route

--- backend/api/src/main.rs
+++ backend/api/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use app::index;
+
+#[macro_use] extern crate rocket;
+
+mod app;
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build()
+        
+        // <-------- ROUTES --------->
+        .mount("/", routes![index])
+
+
+        // launching
+        .launch().await?;
+
+    Ok(())
 }
